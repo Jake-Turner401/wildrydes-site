d09b5d315631fe0d0c861c3e0890032999308896
jake <jake@quantrolox.com>
2022-11-27 16:44:52 +0000

    new_condig

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_j2509o10k', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1kss09aku0ehtb0vpm8rp7udbs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
